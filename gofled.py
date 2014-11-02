c8af9b4bbb9f8547e541240d3bc09bb2baf88f31
wxv <wxv@users.noreply.github.com>
2014-11-02 16:27:53 -0500

    Super golf

--- gofled.py
+++ gofled.py
@@ -1,27 +1,24 @@
-# Golfed mini-intepreter (no debug, cell_max=256)
-# Bytes: 421
+# Golfed mini-intepreter (no eval)
+# Bytes: 330
 # Techniques: Replaced char matching with index y, replaced conditional addition  
-# with adding boolean values (x+(a==2)), variable renaming, using variables for
-# repeated expressions
+# with adding boolean values (x+(a==2)), variable renaming and replacing 
+# repeated expressions. Note: z is 1 or -1
 
 # Global parameters
-F="examples/multiply2.b"
-C=256
+F = "examples/multiply2.b"
+C = 256
+T = 30000
 #----------------------------
 with open(F,'r')as z:p=z.read()
-t=[0]*30000
+t=[0]*T
 i=d=l=0
+y="><+-.,[]"
 while i<len(p):
- f=1;c=p[i];x=t[d];y="><+-.,[]";a=y.index(c)if c in y else 9;d+=(a==0)-(a==1)
- if a in(2,3):t[d]=(x+(a==2)-(a==3))%C
- if a==4:print(chr(x),end="")
+ c=p[i];x=t[d];a=y.index(c)if c in y else-1;t[d]=(x+(a==2)-(a==3))%C;d+=(a==0)-(a==1)
+ if a==4:print(chr(x),end='')
  if a==5:t[d]=ord(input())
- if(a,x)==(6,0):
-  f=0;l=1
-  while l>0:i+=1;c=p[i];l+=(c=="[")-(c=="]")
-  i+=1         
- if a==7 and x!=0:
-  f=0;l=1
-  while l>0:i-=1;c=p[i];l-=(c=="[")-(c=="]")
-  i+=1
- if f:i+=1
+ if(a,x)==(6,0)or(a==7 and x!=0):
+  z=13-2*a;l=1
+  while l>0:i+=z;c=p[i];l+=z*((c=="[")-(c=="]"))
+  i-=1
+ i+=1
