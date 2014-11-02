e946bae178d4bc3912e04d4a3ac0434e2cbc5145
wxv <wxv@users.noreply.github.com>
2014-11-01 23:57:31 -0400

    More golf

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Brainfuck in Python
 
-A simple interpreter and test files are included.
+A simple interpreter, extremely golfed version, and test files are included.
 
 To open the test files, replace the name in interpreter.py with the name of the file to be opened. 
 Most of them are from esolangs.org wiki. The ones with explanations are probably mine.
--- gofled.py
+++ gofled.py
@@ -1,42 +1,27 @@
 # Golfed mini-intepreter (no debug, cell_max=256)
-# Bytes: 
+# Bytes: 445
 
 # Vars
-F = "examples/multiply2.b"
-C = 256
-
+F="examples/multiply2.b"
+C=256
 #----------------------------
 with open(F,'r')as z:p=z.read()
 t=[0]*30000
-i =0
-d = 0
-l = 0
-while i < len(p):
-    f = True  
-    c = p[i]
-    x= t[d]
-    if c == ">":d += 1
-    if c == "<":d -= 1
-    if c == "+":t[d] = (x+ 1) % C
-    if c == "-":t[d] = (x- 1) % C
-    if c == ".":print(chr(x), end="")
-    if c == ",":t[d] = ord(input())         
-    if c == "[" and x== 0:
-        f = False        
-        l = 1     
-        while l > 0:
-            i += 1
-            c = p[i]
-            if c == "[":l += 1
-            if c == "]":l -= 1
-        i += 1         
-    if c == "]" and x!= 0:
-        f = False
-        l = 1
-        while l > 0:
-            i -= 1
-            c = p[i]
-            if c == "[":l -= 1
-            if c == "]":l += 1          
-        i += 1
-    if f:i += 1
+i=d=l=0
+while i<len(p):
+ f=1;c=p[i];x=t[d];y="><+-.,[]";a=y.index(c) if c in y else 9;d+=(a==0);d-=(a==1)
+ if a==2:t[d]=(x+1)%C
+ if a==3:t[d]=(x-1)%C
+ if a==4:print(chr(x),end="")
+ if a==5:t[d]=ord(input())
+ if(a,x)==(6,0):
+  f=0;l=1
+  while l>0:
+   i+=1;c=p[i];l+=(c=="[");l-=(c=="]")
+  i+=1         
+ if a==7 and x!=0:
+  f=0;l=1
+  while l>0:
+   i-=1;c=p[i];l-=(c=="[");l+=(c=="]") 
+  i+=1
+ if f:i+=1
