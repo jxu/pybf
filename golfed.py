9462bb4d3d27ebe6c893a3f550b3c7cd0fcf9297
wxv <wxv@users.noreply.github.com>
2018-03-03 03:13:03 -0500

    Golf both versions

--- golfed.py
+++ golfed.py
@@ -1,5 +1,5 @@
 # Golfed mini-intepreter (no exec)
-# Bytes: 298
+# Bytes: 291
 # Techniques: Replaced char matching with index y, replaced conditional addition  
 # with adding boolean values (x+(a==2)), variable renaming and replacing 
 # repeated expressions. Note: z is 1 or -1
@@ -14,13 +14,12 @@ t=[0]*T
 i=d=l=0
 y="><+-.,[]"
 while i<len(p):
- c=p[i];x=t[d];a=9
+ c=p[i];x=t[d];a=9;l=1
  if c in y:a=y.index(c)
  t[d]=(x+(a==2)-(a==3))%C;d+=(a==0)-(a==1)
  if a==4:print(chr(x),end='')
  if a==5:t[d]=ord(input())
  if a-6==(x!=0):
-  z=13-2*a;l=1
+  z=13-2*a
   while l>0:i+=z;c=p[i];l+=z*((c=="[")-(c=="]"))
-  i-=1
  i+=1
--- golfed2.py
+++ golfed2.py
@@ -1,6 +1,8 @@
 # Golfed mini-intepreter
-# Bytes: 307
+# Bytes: 294
 # Overflow undefined
+# For finding matching [ ] braces, replace + with -
+# Use bool string multiplication to remove strings
 
 # Global parameters
 F = "examples/multiply2.b"
@@ -9,13 +11,13 @@ T = 30000
 #----------------------------
 p=open(F).read()
 t=[0]*T
-i=d=l=0
+i=d=0
 y="><+-.,[]"
-w="l=1;\n while l>0:i+=z;c=p[i];l+=z*((c=='[')-(c==']'))\n i-=1"
+w="while l>0:i+=1;c=p[i];l+=(c=='[')-(c==']')"
 while i<len(p):
- c=p[i];x=t[d];a=8
+ c=p[i];x=t[d];a=8;l=1
  if c in y:a=y.index(c)
- exec(("d+=1|d-=1|t[d]+=1|t[d]-=1|print(chr(x),end='')|t[d]=ord(input())|if x==0:\n z=1;"+w+"|if x:\n z=-1;"+w+"|1").split('|')[a])
+ exec(("d+=1|d-=1|t[d]+=1|t[d]-=1|print(chr(x),end='')|t[d]=ord(input())|"+(x==0)*w+"|"+(x!=0)*w.replace('+','-')+"|1").split('|')[a])
  i+=1
 
 
